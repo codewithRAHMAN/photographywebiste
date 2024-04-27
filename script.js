2a7a1d1d4a52c98ab09b1f236fe425d5cf226781
codewithRAHMAN <153289357+codewithRAHMAN@users.noreply.github.com>
2024-04-28 01:42:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+document.getElementById("knowMoreButton1").addEventListener("click", function() {
+    // Scroll to the main-page section
+    document.getElementById("main-page").scrollIntoView({ behavior: "smooth" });
+  });
+  
+
+  document.getElementById("knowMoreButton2").addEventListener("click", function() {
+    // Scroll to the main-page section
+    document.getElementById("main-page").scrollIntoView({ behavior: "smooth" });
+  });
+  
+  
+  document.getElementById("knowMoreButton3").addEventListener("click", function() {
+    // Scroll to the main-page section
+    document.getElementById("main-page").scrollIntoView({ behavior: "smooth" });
+  });
+  
